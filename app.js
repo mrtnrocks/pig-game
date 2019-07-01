678044f729d4c7cbd3203f16b50b43b74669d331
mrtn.codes <mrtn.glch@tuta.io>
2019-07-01 14:12:44 +0200

    3.8 done

--- app.js
+++ app.js
@@ -13,17 +13,14 @@ var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+activePlayer = 0;
  
 document.querySelector('.dice').style.display = 'none';
 
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1 ').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
@@ -36,5 +33,35 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
 
+    // 3. Update the round score IF the rolled number was not a 1
+    if (dice !==1) {
+        //Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        // Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        /*****
+         * Same with IF
+         * if (activePlayer === 0) {
+         *      activePlayer = 1;
+         * } else {
+         *  	activePlayer = 0;
+         * }
+         * 
+         */
+        roundScore = 0;
 
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        //document.querySelector('.player-0-panel').classList.remove('active');
+        //document.querySelector('.player-1-panel').classList.add('active');
+
+        document.querySelector('.dice').style.display = 'none';
+
+    }
 });
